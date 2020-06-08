f3f210c1ec0052059c5f7b94b03d6094b5b3ff63
Si Young Byun <siyoung.byun@gmail.com>
2020-06-08 19:10:04 +0000

    string exercises done

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let i;
+    let result = "";
+    for (i = 0; i < times; i++) {
+        result = result + word;
+    }
+    return result;
 }
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let phraseArray = phrase.split("");
+    let result = "";
+    for (let i = phraseArray.length - 1; i >= 0; i--) {
+        result = result + phraseArray[i];
+    }
+    return result;
 }
 
 module.exports = reverseString
