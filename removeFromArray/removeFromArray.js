efd5421035666d96939216e181d73bcce08bd3ed
Si Young Byun <siyoung.byun@gmail.com>
2020-06-08 20:19:07 +0000

    removeFromArray done

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rest) {
+    for (let element of rest) {
+        let index = arr.indexOf(element);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 }
 
 module.exports = removeFromArray
