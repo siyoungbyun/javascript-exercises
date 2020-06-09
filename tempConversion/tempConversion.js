22689dcda872555722b78472f2fc2b47df10239c
Si Young Byun <siyoung.byun@gmail.com>
2020-06-09 00:17:36 +0000

    temp conversion done

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let celcius = parseFloat(((fahrenheit - 32) * 5 / 9).toFixed(1));
+  return celcius;
 }
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let fahrenheit = parseFloat((celcius * 9 / 5 + 32).toFixed(1));
+  return fahrenheit;
 }
 
 module.exports = {
