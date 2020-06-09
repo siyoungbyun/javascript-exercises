fa867ae98d8796cc6bffbce3989b6000ff409584
Si Young Byun <siyoung.byun@gmail.com>
2020-06-09 00:01:30 +0000

    sum all done

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(first, second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second)) {
+        return "ERROR";
+    }
+    if (first < 0 || second < 0) {
+        return "ERROR";
+    }
+    let result = 0;
+    let min;
+    let max;
+    if (first >= second) {
+        min = second;
+        max = first;
+    } else {
+        min = first;
+        max = second;
+    }
+    for (let i = min; i < max + 1; i++) {
+        result = result + i;
+    }
+    return result;
 }
 
 module.exports = sumAll
